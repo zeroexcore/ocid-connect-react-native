d5dac2e82a04e632f63d2ff084bccfa7f358156b
Alex Astro <ariaworkshopnft@gmail.com>
2025-09-30 11:08:42 +0300

    remove react components from sdk

--- src/react/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginButton } from './LoginButton';
-export { default as LoginCallBack } from './LoginCallBack';
-export { default as OCConnect } from './OCConnect';
-export * from './OCContext';
